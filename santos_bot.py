3b4410e33cf806444980034227723a759c1735c8
Marat <124066267+maramaslovQA@users.noreply.github.com>
2023-03-01 18:39:16 +0100

    Update santos_bot.py

--- santos_bot.py
+++ santos_bot.py
@@ -1,3 +1,4 @@
+
 import telebot
 
 TOKEN = '6175249815:AAETXvUKfPMa0TPtYj-ldmvU1Bk7EA1sIBs'
@@ -13,39 +14,57 @@ bot = telebot.TeleBot(TOKEN, parse_mode='html') # создание бота
 # обработчик команды '/start'
 @bot.message_handler(commands=['start'])
 def start_command_handler(message):
-    sti = open ('lapenko_zdravo.webp', 'rb')
+    sti = open('lapenko_zdravo.webp', 'rb')
     bot.send_sticker(message.chat.id, sti)
     # отправляем ответ на команду '/start'
-    bot.send_message(chat_id=message.chat.id,text='Привет {0.first_name}! Я помогу тебе справиться с дофаминовым голоданием \nНажми нужную кнопку 🤓'.format(message.from_user, bot.get_me()), reply_markup=markup)   
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton('Сериал 📺')
-    item2 = types.KeyboardButton('Мем 🤡')
-    item3 = types.KeyboardButton('Написать создателю 📝')
-    markup.add(item1,item2,item3)
+    bot.send_message(
+        chat_id=message.chat.id,  # id чата, в который необходимо направить сообщение
+        text='Привет {0.first_name}! Я помогу тебе справиться с дофаминовым голоданием \nНажми нужную кнопку 🤓'.format(
+            message.from_user, bot.get_me()), reply_markup=markup)  # текст сообщения
+
+
+# Кнопки
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+btn1 = types.KeyboardButton('Сериал 📺')
+btn2 = types.KeyboardButton('Мем 🤡')
+btn3 = types.KeyboardButton('Написать создателю 📝')
+markup.add(btn1, btn2, btn3)
 
 #actions for keyboard
 @bot.message_handler(content_types = ['text'])
 def activity(message):
         if message.text == 'Сериал 📺':
-            bot.send_message(message.chat.id,show+str(random.choice(sitcom_list)))
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            item1 = types.KeyboardButton('Ок, попробую')
-            item2 = types.KeyboardButton('Смотрел, давай другой')
-            markup.add(item1,item2)
+          bot.send_message(message.chat.id,show+str(random.choice(sitcom_list)))
+          markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+          btn1 = types.KeyboardButton('Ок, попробую')
+          btn2 = types.KeyboardButton('Смотрел, давай другой')
+          markup.add(btn1,btn2)
+          bot.send_message(message.chat.id,text='м ?',reply_markup=markup)
+        elif message.text == 'Смотрел, давай другой':
+          bot.send_message(message.chat.id,show+str(random.choice(sitcom_list)))
+          bot.send_message(message.chat.id,text="А этот?")
             
         elif message.text == 'Мем 🤡':
             bot.send_message(message.chat.id,mem+str(random.randint(35,6000)))
             
         elif message.text == 'Написать создателю 📝':
             bot.send_message(message.chat.id,'t.me/maramaslov')
-            
+                   
         elif message.text == 'Ок, попробую':
-            bot.send.video(message,chat.id, 'https://media.giphy.com/media/jtQpRa3y7S2Ke3JvQE/giphy.gif', None, 'Text')
-            
-        elif message.text == 'Смотрел, давай другой':
-            bot.send_message(message.chat.id,"А этот ?\n\n{0}".format(random.choice(sitcom_list)))
-            
-            
+            bot.send_video(message.chat.id, 'https://media.giphy.com/media/jtQpRa3y7S2Ke3JvQE/giphy.gif', None, 'Text') 
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton('Мем 🤡')
+            btn2 = types.KeyboardButton('На главную ⤴️')
+            markup.add(btn1,btn2)
+            bot.send_message(message.chat.id,text='Приятного просмотра🍿',reply_markup=markup)  
+
+        elif message.text == 'На главную ⤴️':
+          markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+          btn1 = types.KeyboardButton('Сериал 📺')
+          btn2 = types.KeyboardButton('Мем 🤡')
+          btn3 = types.KeyboardButton('Написать создателю 📝')
+          markup.add(btn1, btn2, btn3)
+          bot.send_message(message.from_user.id, 'Йо', reply_markup=markup)
         else:
             bot.send_message(message.chat.id,'Я пока не понимаю текст, сорян 😕 тыкай кнопку')
 
@@ -58,4 +77,3 @@ def main():
 if __name__ == '__main__':
     main()
     
-
